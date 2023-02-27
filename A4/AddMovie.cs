f8e4aa2d49d69913b01641f509351273a312fa6d
Jos <josdiaz95@outlook.com>
2023-02-27 15:03:59 -0600

    Add project files.

--- A4/AddMovie.cs
+++ A4/AddMovie.cs
@@ -27,37 +27,38 @@ namespace A4
 
         public void CreateNewMovieTitle()
         {
-           
-            string file = "movies.csv";
+            try
+            {
+                string file = "movies.csv";
 
-            List<string> genreList = new List<string>();
+                List<string> genreList = new List<string>();
 
-            //genre looping
-            bool loopGenres = true; 
+                //genre looping
+                bool loopGenres = true;
 
-             //checking if movie file exist or not
-             bool exist = false;
+                //checking if movie file exist or not
+                bool exist = false;
 
-             //string reader for year
-             string year;
+                //string reader for year
+                string year;
 
-             //Try adding a movie
+                //Try adding a movie
 
                 Console.WriteLine("Enter movie title");
                 string newMovie = FirstLetterCap(Console.ReadLine());
                 Console.WriteLine("Add movie year");
                 year = Console.ReadLine();
 
-            //to remove "the" to check for equals without ruining user input
+                //to remove "the" to check for equals without ruining user input
                 string compareMovie = newMovie;
 
                 string isThe = FirstLetterCap("the");
                 if (compareMovie.Contains(isThe))
                 {
-                    compareMovie = compareMovie.Remove(0,4);
+                    compareMovie = compareMovie.Remove(0, 4);
                 }
 
-            using (var reader = new StreamReader(file))
+                using (var reader = new StreamReader(file))
                 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
                 {
 
@@ -74,7 +75,7 @@ namespace A4
                             exist = true;
                             break;
                         }
-                        
+
                     }
 
                     if (!exist)
@@ -117,62 +118,69 @@ namespace A4
                         }
                     }
                 }
-            
-
-            //generating next id
-            int nextId;      
-
-            using (var reader = new StreamReader(file))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                var records = csv.GetRecords<MenuLibrary>().Last();
-                int lastId = Int32.Parse(records.movieId);
-                nextId = 1;
-                nextId += lastId;
-
-            }
 
 
-            //writing list to add to the file
-            var movieRecords = new List<toFile>
+                //generating next id
+                int nextId;
+
+                using (var reader = new StreamReader(file))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<MenuLibrary>().Last();
+                    int lastId = Int32.Parse(records.movieId);
+                    nextId = 1;
+                    nextId += lastId;
+
+                }
+
+
+                //writing list to add to the file
+                var movieRecords = new List<toFile>
             {
                  new toFile { movieId = nextId, title = newMovie + " " + $"({year})", genres =(string.Join("|",genreList)) }
             };
 
-            //capitalizing the first letter of every word
-             static string FirstLetterCap(string str1)
-            {
-                return string.Join(" ", str1.Split(' ').Select(str1 => char.ToUpper(str1[0]) + str1.Substring(1)));
-            }
-            
-
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-            };
-            
-            using (var stream = File.Open(file,FileMode.Append))
-
-            using (var writer = new StreamWriter(stream))
-
-            using (var csv = new CsvWriter(writer, config))
-            {
-                //if movie doesn't exist add to file --add to file
-                if (!exist)
+                //capitalizing the first letter of every word
+                static string FirstLetterCap(string str1)
                 {
-                    csv.WriteRecords(movieRecords);
+                    return string.Join(" ", str1.Split(' ').Select(str1 => char.ToUpper(str1[0]) + str1.Substring(1)));
                 }
 
-            }
 
-            //Getting total number of movies
-            using (var reader = new StreamReader(file))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false,
+                };
+
+                using (var stream = File.Open(file, FileMode.Append))
+
+                using (var writer = new StreamWriter(stream))
+
+                using (var csv = new CsvWriter(writer, config))
+                {
+                    //if movie doesn't exist add to file --add to file
+                    if (!exist)
+                    {
+                        csv.WriteRecords(movieRecords);
+                    }
+
+                }
+
+                //Getting total number of movies
+                using (var reader = new StreamReader(file))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var count = csv.GetRecords<toFile>().ToList();
+                    System.Console.WriteLine($"The total number of movies are: {count.Count()}\n");
+
+                }
+            }
+            catch (Exception e)
             {
-                var count = csv.GetRecords<toFile>().ToList();
-                System.Console.WriteLine($"The total number of movies are: {count.Count()}\n");
+                Console.WriteLine("There is  no file to add to.");
                 
             }
+           
 
         }
 
--- A4/AllMovies.cs
+++ A4/AllMovies.cs
@@ -28,76 +28,81 @@ namespace A4
         }
 
     }
-  
+
 
     public class AllMovies
     {
         public void DisplayMoviesContent()
         {
-            //searching for a movie with a keyword or tittle
-
-            bool found = false;
-            string file = "movies.csv";
-            Console.WriteLine("Enter a keyword/title to search for a movie"); 
-            string movieSearch = Console.ReadLine().ToUpper();
-
-
-
-            while(movieSearch.Length < 1)
+            try
             {
-                Console.WriteLine("***You must enter a movie title or keyword!***\n");
+                //searching for a movie with a keyword or tittle
+
+                bool found = false;
+                string file = "movies.csv";
                 Console.WriteLine("Enter a keyword/title to search for a movie");
-                movieSearch = Console.ReadLine().ToUpper();
-            }
-           
+                string movieSearch = Console.ReadLine().ToUpper();
 
-            if (movieSearch.Length > 1)
-            {
+
+
+                while (movieSearch.Length < 1)
                 {
-
-                    string isThe = "the".ToUpper();
-                    Console.WriteLine("-----------------");
-                    Console.WriteLine($"Searching with word: {movieSearch}\n");
-
-                    if (movieSearch.Contains(isThe))
-                    {
-                        movieSearch = movieSearch.Replace("THE", "");
-                    }
-
-                    
-                    using (var reader = new StreamReader(file))
-                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-                    {
-
-                        var movies = csv.GetRecords<MenuLibrary>().ToList();
-
-                        foreach (var mt in movies)
-                        {
-                            var titles = mt.movieId + "-- " + mt.title.ToUpper() + "," + mt.genres;
-
-                            if (titles.Contains(movieSearch))
-                            {
-                                found = true;
-                                Console.WriteLine(titles);
-                            }
-
-                        }
-                    }
-                    if (!found)
-                    {
-                        Console.WriteLine("Uh oh! Movie(s) not found.");
-                    }
-
+                    Console.WriteLine("***You must enter a movie title or keyword!***\n");
+                    Console.WriteLine("Enter a keyword/title to search for a movie");
+                    movieSearch = Console.ReadLine().ToUpper();
                 }
-            }
-          
 
-            Console.WriteLine("");
-            Console.WriteLine("*************************************************\n");
-        
+
+                if (movieSearch.Length > 1)
+                {
+                    {
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine($"Searching with word: {movieSearch}\n");
+
+                        if (movieSearch.Contains("THE"))
+                        {
+                            movieSearch = movieSearch.Replace("THE", "");
+                        }
+
+
+                        using (var reader = new StreamReader(file))
+                        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                        {
+
+                            var movies = csv.GetRecords<MenuLibrary>().ToList();
+
+                            foreach (var mt in movies)
+                            {
+                                var titles = mt.movieId + "-- " + mt.title.ToUpper() + "," + mt.genres;
+
+                                if (titles.Contains(movieSearch))
+                                {
+                                    found = true;
+                                    Console.WriteLine(titles);
+                                }
+
+                            }
+                        }
+
+                        if (!found)
+                        {
+                            Console.WriteLine("Uh oh! Movie(s) not found.");
+                        }
+
+                    }
+                }
+
+
+                Console.WriteLine("");
+                Console.WriteLine("*************************************************\n");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Sorry, File doesn't exit." + e);
+            }
         }
     }
-    
 }
 
 
